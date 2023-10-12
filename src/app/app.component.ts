4609374fd7edcd9d7ebfeb1e21ac8c444a1540cc
bruno-boggio <brn_boggio@hotmail.com>
2023-10-12 19:24:20 -0300

    botao criar tópico, formulário e aguardando feedback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 
 export class AppComponent {
@@ -22,6 +22,36 @@ export class AppComponent {
   criarTopico() {
     const novoTopico = `Tópico ${this.topicos.length + 1}`;
     this.topicos.push(novoTopico);
+  }
+
+  mostrarDiv = false;
+  mostrarForm = false;
+
+  showDiv = true;
+
+  showTopic = false;
+
+  divComments = false;
+
+  toggleForm() {
+    this.mostrarDiv = true;
+    this.showDiv = false;
+  }
+
+
+  enviarFormulario(){
+    this.mostrarDiv = false;
+    return false;
+  }
+
+  newTopic(){
+    this.showTopic = true;
+  }
+
+  showDivComments(){
+    this.mostrarDiv = false;
+    this.divComments = true;
+    this.showTopic = true;
   }
 
 
